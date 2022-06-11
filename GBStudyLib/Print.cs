1660d95bdf9f9bd764e19a9b7a9f71563c21fff0
Alexey Kim <mammamia008@mail.ru>
2022-06-11 11:48:47 +0300

    Задание 3

--- GBStudyLib/Print.cs
+++ GBStudyLib/Print.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 // Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause). 
 // Студент Ким Алексей
 
@@ -15,6 +16,69 @@ namespace GBStudyLib
 
           }
 
+     
+    }
+
+    public class Formulas
+    {
+       public bool IsOdd(int n)
+        {
+
+            return n % 2 == 0;
+
+
+        }
+        public int NumberSumm(int n)
+        {
+            int s = 0;
+            while (n != 0)
+            {
+                s = s + n % 10;
+                n = n / 10; 
+            }
+            return s;
+        }
+        public bool IsGood(int n)
+        {
+           if (n % NumberSumm(n) == 0) 
+                return true;
+           else return false;
+        }
+
+         
+
+        }
+    public class Complex
+    {
+        
+            public double im;
+            public double re;
+
+
+            public Complex Minus(Complex x)
+            {
+                Complex y = new Complex();
+                y.im = x.im - im;
+                y.re = x.re - re;
+                return y;
+            }
+
+
+            public Complex Multi(Complex x)
+            {
+                Complex y = new Complex();
+                y.im = re * x.im + im * x.re;
+                y.re = re * x.re + im * x.im;
+                return y;
+            }
+            public string ToString()
+            {
+                return $"{re} + {im} * i";
+            }
+
+       
+
 
     }
+
 }
